cec530b22b0c7b46177b09bb45d9af6627ec52ca
rajnish523 <rajnish.kumar@firstperformance.com>
2023-03-23 17:19:39 +0530

    firs

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask,jsonify
+
+
+app = Flask(__name__) # Flask constructor
+
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/')	
+def hello():
+	return 'HELLO'
+
+if __name__=='__main__':
+    app.run()
